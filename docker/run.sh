bdede97d929545ee081107776d0efc11c69dce35
Kkun84 <kesuike84@gmail.com>
2020-12-26 22:36:54 +0900

    実行ファイルの追加

--- docker/run.sh
+++ docker/run.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-# docker run \
-#     -d \
-#     --init \
-#     --rm \
-#     -p 8889:8888 \
-#     -it \
-#     --gpus=all \
-#     --ipc=host \
-#     --name=PatchEnv \
-#     --env-file=.env \
-#     --volume=$PWD:/workspace \
-#     --volume=$DATASET:/dataset \
-#     patch_env:latest \
-#     ${@-fish}
 docker run \
     -d \
     --init \
     --rm \
+    -p 5000:5000 \
+    -p 6006:6006 \
     -p 8501:8501 \
+    -p 8502:8502 \
+    -p 8503:8503 \
+    -p 8888:8888 \
     -it \
     --gpus=all \
     --ipc=host \
